07a318393f3bae0c6e1618e694d17f08c10f495f
Wesley.antunesdc <wesleyantunesdca@gmail.com>
2021-03-01 10:06:23 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/aprendaingles/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.aprendaingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.aprendaingles.fragmentos.AnimaisFragment;
+import com.example.aprendaingles.fragmentos.NumerosFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Animais", AnimaisFragment.class).add("Números", NumerosFragment.class).create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
