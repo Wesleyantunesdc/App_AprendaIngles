9cb2e12384c9ce4d7e04264b611601265b38c5ef
Wesley.antunesdc <wesleyantunesdca@gmail.com>
2021-03-01 12:44:25 -0300

    Finalização do app

--- app/src/main/java/com/example/aprendaingles/fragmentos/AnimaisFragment.java
+++ app/src/main/java/com/example/aprendaingles/fragmentos/AnimaisFragment.java
@@ -1,12 +1,16 @@
 package com.example.aprendaingles.fragmentos;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
@@ -15,7 +19,7 @@ import com.example.aprendaingles.R;
  * Use the {@link AnimaisFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AnimaisFragment extends Fragment {
+public class AnimaisFragment<ImageButtom> extends Fragment implements View.OnClickListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +29,8 @@ public class AnimaisFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ImageButton cao,gato,ovelha,vaca,leao,macaco;
+    private MediaPlayer mediaPlayer;
 
     public AnimaisFragment() {
         // Required empty public constructor
@@ -60,7 +66,68 @@ public class AnimaisFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animais, container, false);
+        View view = inflater.inflate(R.layout.fragment_animais, container, false);
+        cao = view.findViewById(R.id.imageButtonCao);
+        gato = view.findViewById(R.id.imageButtonGato);
+        macaco = view.findViewById(R.id.imageButtonMacaco);
+        leao = view.findViewById(R.id.imageButtonLeao);
+        ovelha = view.findViewById(R.id.imageButtonOvelha);
+        vaca = view.findViewById(R.id.imageButtonVaca);
+
+        cao.setOnClickListener(this);
+        gato.setOnClickListener(this);
+        macaco.setOnClickListener(this);
+        leao.setOnClickListener(this);
+        ovelha.setOnClickListener(this);
+        vaca.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("Elemento clicado", "Item: "+ v.getId());
+        switch (v.getId()){
+            case R.id.imageButtonCao:
+                tocarSom(R.raw.dog);
+                break;
+            case R.id.imageButtonGato:
+                tocarSom(R.raw.cat);
+                break;
+            case R.id.imageButtonLeao:
+                tocarSom(R.raw.lion);
+                break;
+            case R.id.imageButtonMacaco:
+                tocarSom(R.raw.monkey);
+                break;
+            case R.id.imageButtonOvelha:
+                tocarSom(R.raw.sheep);
+                break;
+            case R.id.imageButtonVaca:
+                tocarSom(R.raw.cow);
+                break;
+        }
+    }
+
+    public void tocarSom(int id){
+        mediaPlayer = MediaPlayer.create(getContext(),id);
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+        }
     }
 }
--- app/src/main/java/com/example/aprendaingles/fragmentos/NumerosFragment.java
+++ app/src/main/java/com/example/aprendaingles/fragmentos/NumerosFragment.java
@@ -1,5 +1,6 @@
 package com.example.aprendaingles.fragmentos;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
@@ -15,7 +17,7 @@ import com.example.aprendaingles.R;
  * Use the {@link NumerosFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NumerosFragment extends Fragment {
+public class NumerosFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +27,8 @@ public class NumerosFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ImageButton numero1,numero2,numero3,numero4,numero5,numero6;
+    private MediaPlayer mediaPlayer;
 
     public NumerosFragment() {
         // Required empty public constructor
@@ -60,7 +64,66 @@ public class NumerosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeros, container, false);
+        View view  = inflater.inflate(R.layout.fragment_numeros, container, false);
+        numero1 = view.findViewById(R.id.imageButton1);
+        numero2 = view.findViewById(R.id.imageButton2);
+        numero3 = view.findViewById(R.id.imageButton3);
+        numero4 = view.findViewById(R.id.imageButton4);
+        numero5 = view.findViewById(R.id.imageButton5);
+        numero6 = view.findViewById(R.id.imageButton6);
+
+        numero1.setOnClickListener(this);
+        numero2.setOnClickListener(this);
+        numero3.setOnClickListener(this);
+        numero4.setOnClickListener(this);
+        numero5.setOnClickListener(this);
+        numero6.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageButton1:
+                tocarSom(R.raw.one);
+                break;
+            case R.id.imageButton2:
+                tocarSom(R.raw.two);
+                break;
+            case R.id.imageButton3:
+                tocarSom(R.raw.three);
+                break;
+            case R.id.imageButton4:
+                tocarSom(R.raw.four);
+                break;
+            case R.id.imageButton5:
+                tocarSom(R.raw.five);
+                break;
+            case R.id.imageButton6:
+                tocarSom(R.raw.six);
+                break;
+        }
+    }
+
+    public void tocarSom(int id){
+        mediaPlayer = MediaPlayer.create(getContext(),id);
+        if(mediaPlayer!=null){
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+        }
     }
 }
